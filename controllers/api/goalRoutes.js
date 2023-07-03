d2fc793b1b5700a07b6d3d2f59dc1fe4246ef602
Hailey Rae <haileyraehughes89@gmail.com>
2023-07-02 21:46:19 -0500

    updating goalRoutes to find goals by user and correcting syntax issue in goal-seeds

--- controllers/api/goalRoutes.js
+++ controllers/api/goalRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Goal = require("../../models/Goal");
+const User = require("../../models/User");
 
 //get route for all goals
 router.get("/", async (req, res) => {
@@ -27,6 +28,28 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(goalData);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+// Get route for goals by user
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const goals = await Goal.findAll({
+      where: { userId },
+    });
+
+    res.json(goals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
