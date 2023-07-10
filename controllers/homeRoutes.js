e2020be94eef8b82411b521f8a4f0033e6b6faaa
Hailey Rae <haileyraehughes89@gmail.com>
2023-07-10 11:52:45 -0500

    removed authentification so user can access homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const checkAuthenticated = (req, res, next) => {
-  if (req.session.logged_in) {
-    next();
-  } else {
-    res.redirect("/nonauth");
-  }
-};
+router.get("/homepage", (req, res) => {
+  res.render("homepage");
+});
 
-router.get("/login", checkAuthenticated, (req, res) => {
+router.get("/login", (req, res) => {
   res.render("login");
 });
 
 router.get("/nonauth", (req, res) => {
   res.render("nonauth");
-});
-
-router.get("/homepage", checkAuthenticated, (req, res) => {
-  res.render("homepage");
 });
 
 router.get("/goals", (req, res) => {
